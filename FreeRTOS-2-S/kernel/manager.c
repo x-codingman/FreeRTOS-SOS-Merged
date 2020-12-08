d68241f6c2f7abc0a7ec513406c36be0384b6236
x-codingman <xinhuishao1996@gmail.com>
2020-12-08 09:54:56 +0800

    add tag v1.0

--- FreeRTOS-2-S/kernel/manager.c
+++ FreeRTOS-2-S/kernel/manager.c
@@ -22,19 +22,19 @@
 
 
 
-#define MODULES_MAX 10 // max amount of aviliable modules
+#define MODULES_MAX 10 										// Max amount of aviliable modules
 #define SOS_KERNEL_STACK_SIZE 200
 #define SOS_MODULE_STACK_SIZE 1024
 
 
-Module_t modules[MODULES_MAX];   // All modules
-static uint32_t modules_index=0;  // Modules  used
+Module_t modules[MODULES_MAX];   							// All modules
+static uint32_t modules_index=0;							// Modules  used
 static StackType_t * kernel_stack_pointer;
 StackType_t kernel_stack[SOS_KERNEL_STACK_SIZE];       
-struct CallModuleFrame head; // The head of the call_module_frame list, note it only used as an index.
+struct CallModuleFrame head; 								// The head of the call_module_frame list, note it only used as an index.
 struct CallModuleFrame* call_moudle_frame_head=&head;
 
-static uint32_t call_ID=1;                                 // this is used to generate the call_ID for each module calling
+static uint32_t call_ID=1;                                  // this is used to generate the call_ID for each module calling
                         
 
 void init_stack(){
@@ -57,90 +57,64 @@ SOS_Result_t SOS_invoke_command(
     
     // Look up the module
     int i=0;
-    for(;i<modules_index;i++){
-        if(modules[i].module_id==module_id){
+	for(;i<modules_index;i++){
+    	if(modules[i].module_id==module_id){
             
 
             
-            //TODO(x-codingman):
-            // 1.alloc a CallModuleFrame_t
+           
+			// 1.alloc a CallModuleFrame_t
             // 2.insert the cur_call_module_frame to the head 
             // 3. alloc the module stack 
             // 4. save the kernel stack 
-           //cur_call_moudle_frame=memory_alloc(sizeof(struct CallModuleFrame));
-		   //
-           //if(call_moudle_frame_head!=NULL){
-               //cur_call_moudle_frame->next=call_moudle_frame_head;
-               //call_moudle_frame_head=cur_call_moudle_frame;
-           //}else{
-               //call_moudle_frame_head=cur_call_moudle_frame;
-           //}
-		   call_module_list_add(&cur_call_moudle_frame);
-           cur_call_moudle_frame->cur_call_ID=call_ID;
-           call_ID++;
+            //cur_call_moudle_frame=memory_alloc(sizeof(struct CallModuleFrame));
+		    //
+		    call_module_list_add(&cur_call_moudle_frame);
+            cur_call_moudle_frame->cur_call_ID=call_ID;
+			call_ID++;
 
            
-           StackType_t module_stack_alloced = memory_alloc(SOS_MODULE_STACK_SIZE);
-		   module_stack=module_stack_alloced+SOS_MODULE_STACK_SIZE;
-		   module_stack = ROUNDDOWN(module_stack,DOUBLEWORD);
-		   cur_call_moudle_frame->module_stack=module_stack_alloced;
+            StackType_t module_stack_alloced = memory_alloc(SOS_MODULE_STACK_SIZE);
+		    module_stack=module_stack_alloced+SOS_MODULE_STACK_SIZE;
+		    module_stack = ROUNDDOWN(module_stack,DOUBLEWORD);
+		    cur_call_moudle_frame->module_stack=module_stack_alloced;
 		   
-           // save the context of kernel 
-		   __asm volatile(
-				  "	  push {r0-r5}									\n"	// store r0-r3 in the stack
-				  "   push {r4-r5}     								\n" // Store r4 in the stack
-				  "   mov r4, %0                                    \n" // Move the pointer to cur_call_moudle_frame to r4
-				  "   stmia r4!, {r0-r3}                            \n" // Store r0-r3
-				  "   mov r3, r4                                    \n" // Mov  r4 to r3
-				  "   pop {r4-r5}                                      \n" // Restore r4
-				  "   stmia r3!, {r4-r7}                            \n" // Store r4-r7
-				  "	  mov r0, r8									\n" // r0 = r8. 
-				  "	  mov r1, r9									\n" // r1 = r9. 
-				  "	  mov r2, r10									\n" // r2 = r10. 
-				  "	  mov r4, r11									\n" // r3 = r11. 
-				  "	  stmia r3!, {r0-r2,r4}							\n" // Store the high registers that are not saved automatically. 
-				  "	  mrs r0, msp									\n" // r0 = MSP. 
-				  "	  add r0, r0, #24								\n" // Compensate the MSP with 20 bytes because we will pop 5 registers in the following code.
-				  "	  mrs r1, control								\n" // r1 = CONTROL. 
-				  "	  ldr r2, =sys_call_return_label				\n" // r2 = sys_call_return.
-				  "	  mov r4, #1									\n" 
-				  "	  orr r2, r2, r4								\n" // Make sure that the sys_call_return_label address is odd.(Thumb mode) 
-				  "	  mov r4, lr									\n"	// r4 = lr
-				  "	  mrs r5, psp									\n"
-				  "	  stmia r3!, {r0-r2,r4-r5}						\n" /* Store MSP, CONTROL ,module_return_address and LR on the stack. */
-				  "	  pop {r0-r5}									\n"	// restore r0-r3
-					::"r"(cur_call_moudle_frame)
-					:"memory"
-			);
-		
-	
-
-			// Store call_ID
-			// Set the module_stack to psp 
-		    // __asm volatile(
-			// "   push {r0-r1}									\n"
-			// "	mov r1,	%1										\n"
-			// "	mov r0, %0										\n"
-		    // "	sub r0, r0, #4									\n"
-		    // "	stmia r0!, {r1}									\n"
-		    // "	sub r0, r0, #4									\n"
-		    // "	msr psp, r0										\n"
-			// "	pop {r0-r1}										\n"
-		    // ::"r"(module_stack),"r"(cur_call_moudle_frame->cur_call_ID)
-			// : "memory"
-		    // );
+           	// save the context of kernel 
+			__asm volatile(
+					"	  push {r0-r5}									\n"	// store r0-r3 in the stack
+					"   push {r4-r5}     								\n" // Store r4 in the stack
+					"   mov r4, %0                                    \n" // Move the pointer to cur_call_moudle_frame to r4
+					"   stmia r4!, {r0-r3}                            \n" // Store r0-r3
+					"   mov r3, r4                                    \n" // Mov  r4 to r3
+					"   pop {r4-r5}                                      \n" // Restore r4
+					"   stmia r3!, {r4-r7}                            \n" // Store r4-r7
+					"	  mov r0, r8									\n" // r0 = r8. 
+					"	  mov r1, r9									\n" // r1 = r9. 
+					"	  mov r2, r10									\n" // r2 = r10. 
+					"	  mov r4, r11									\n" // r3 = r11. 
+					"	  stmia r3!, {r0-r2,r4}							\n" // Store the high registers that are not saved automatically. 
+					"	  mrs r0, msp									\n" // r0 = MSP. 
+					"	  add r0, r0, #24								\n" // Compensate the MSP with 20 bytes because we will pop 5 registers in the following code.
+					"	  mrs r1, control								\n" // r1 = CONTROL. 
+					"	  ldr r2, =sys_call_return_label				\n" // r2 = sys_call_return.
+					"	  mov r4, #1									\n" 
+					"	  orr r2, r2, r4								\n" // Make sure that the sys_call_return_label address is odd.(Thumb mode) 
+					"	  mov r4, lr									\n"	// r4 = lr
+					"	  mrs r5, psp									\n"
+					"	  stmia r3!, {r0-r2,r4-r5}						\n" /* Store MSP, CONTROL ,module_return_address and LR on the stack. */
+					"	  pop {r0-r5}									\n"	// restore r0-r3
+						::"r"(cur_call_moudle_frame)
+						:"memory"
+				);
 		   
 			// Call the module entry
 			// This function will not return.
 			call_module( command_id,operation,&modules[i],module_stack);
-			
 			// The sys_call_return_label is used for the syscall to return to the kernel.
 			__asm volatile
 			(
 			"	sys_call_return_label:							\n"
-			
-				
-				"	movs %0, r0 							\n"
+				"	movs %0, r0 								\n"
 			:"=r"(res)
 			);
 			memory_free(module_stack_alloced);
@@ -209,7 +183,7 @@ void call_module( uint32_t command_id,
 		module_stack--;
 		*module_stack = 0;						// r3
 		module_stack--;
-		*module_stack = module->module_entry;			// r2
+		*module_stack = module->module_entry;	// r2
 		module_stack--;
 		*module_stack = operation;				//r1
 		module_stack--;
